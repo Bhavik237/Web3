525d2563f65304f8c3497e4fcc4e30d9d8aa9a41
Bhavik Patel <bhavik.patel@tekditechnologies.com>
2024-08-28 00:27:57 +0530

    Week 1 Assignment

---
+++ Week1/index.js
@@ -0,0 +1,14 @@
+
+// Assignment 1
+//Give me an input string that outputs a SHA-256 hash that starts with 00000 . How will you do it?
+
+
+//Assignment 2
+//What if I ask you that the input string should start with 100xdevs ? How would the code change?
+
+
+//Assignment 3
+//What if I ask you to find a nonce for the following input - 
+// harkirat => Raman | Rs 100
+// Ram => Ankit | Rs 10
+
