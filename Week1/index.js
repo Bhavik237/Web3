d6a0e11485ce1818d94af73b02b6e485a666e8a0
Bhavik237 <bhavikdpatel237@gmail.com>
2024-08-29 23:46:16 +0530

    Week1 Assignment 1

--- Week1/index.js
+++ Week1/index.js
@@ -10,8 +10,20 @@ console.log(crypto.createHash('sha256').update(input).digest('hex'))
 //Give me an input string that outputs a SHA-256 hash that starts with 00000 . How will you do it?
 
 
+function findPrefixWithZero(){
+    let prefix = '0000';
+    let num = 0;
+    while(true){
+        let input = num.toString();
+        let hash = crypto.createHash('sha256').update(input).digest('hex');
+        if(hash.startsWith(prefix)){
+            return {input : input , hash : hash};
+        }
+        num++;
+    }
+}
 
-
+var Assignment = console.log(findPrefixWithZero());
 
 //Assignment 2
 //What if I ask you that the input string should start with 100xdevs ? How would the code change?
